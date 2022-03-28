3ebfe5c9c2d07f9734970e91a4762d51510d91d6
SravanReddy77 <sravanreddy.annadi7777@gmail.com>
2022-03-28 16:24:48 -0400

    Added JUnit Tests

--- src/JUnitTest/JUnitTest.java
+++ src/JUnitTest/JUnitTest.java
@@ -12,12 +12,20 @@ public class JUnitTest {
         System.out.println("Running before Test");
     }
     @Test
-    public void test_add(){
+    public void test_add1(){
      Addition add = new Addition();
      assertEquals(10, add.add(5,5));
      assertNotEquals(50, add.add(45,4));
      System.out.println("Running Addition Test");
     }
+    @Test
+    public void test_add2(){
+        Addition add = new Addition();
+        assertEquals(-10, add.add(-5,-5));
+        assertEquals(50, add.add(45,5));
+        System.out.println("Running Addition Test");
+    }
+
 
     @AfterAll
     public void afterTest(){
---
+++ src/JUnitTest/JUnitTestSuite.java
@@ -0,0 +1,14 @@
+//package JUnitTest;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//
+//
+//
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//        //Provide all the tests you want to run
+//        JUnitTest.class
+//})
+//public class JUnitTestSuite {
+//
+//}
